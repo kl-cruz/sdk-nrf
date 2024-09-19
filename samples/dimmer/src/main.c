4df05dda79fec51c0af0333882f1ece97a5c2496
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2024-09-19 13:24:48 +0200

    Play with the sample
    Signed-off-by: Karol Lasończyk <karol.lasonczyk@nordicsemi.no>

--- samples/dimmer/src/main.c
+++ samples/dimmer/src/main.c
@@ -19,6 +19,14 @@ int main(void)
 	uint8_t dim_value = 0;
 	const struct device *led_nrf;
 	led_nrf = DEVICE_DT_GET(DT_NODELABEL(nrf_led));
+	while (dim_value < 100)
+	{
+		led_set_brightness(led_nrf, 0, dim_value++);
+		k_sleep(K_MSEC(50));
+	}
+
+
+	pm_device_action_run(led_nrf, PM_DEVICE_ACTION_SUSPEND);
 
 	return 0;
 }
